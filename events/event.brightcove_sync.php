a63ebda76b29d05ff5644cf542a03eb8d49ef2d0
Rowan Lewis <me@rowanlewis.com>
2011-07-29 23:16:14 +1000

    Made it Symphony 2.2 compatible, fixed a few issues.

--- events/event.brightcove_sync.php
+++ events/event.brightcove_sync.php
@@ -1,8 +1,8 @@
 <?php
-	
+
 	require_once(EXTENSIONS . '/brightcove/lib/echove.php');
 	require_once(EXTENSIONS . '/symql/lib/class.symql.php');
-	
+
 	class EventBrightcove_Sync extends Event {
 		public static function about(){
 			return array(
@@ -14,21 +14,21 @@
 				),
 				'version'			=> '1.0.1',
 				'release-date'		=> '2010-01-12'
-			);						 
+			);
 		}
-		
-		public function load() {			
+
+		public function load() {
 			return $this->__trigger();
 		}
-		
+
 		protected function __trigger() {
 			// This header is not to be removed.
 			header('content-type: text/plain');
-			
-			$db = Frontend::Database();
-			$conf = Frontend::Configuration();
-			$page = Frontend::Page();
-			$driver = $page->ExtensionManager->create('brightcove');
+
+			$db = Symphony::Database();
+			$conf = Symphony::Configuration();
+			$page = Symphony::Engine()->Page;
+			$driver = Symphony::ExtensionManager()->create('brightcove');
 			$api = $driver->getAPI();
 			$stats = array(
 				'uploading'	=> array(),
@@ -36,13 +36,13 @@
 				'encoding'	=> array(),
 				'completed'	=> array()
 			);
-			
+
 			// Attempt to upload a video x number of times:
 			$max_attempts = 5;
-			
+
 			// Upload x number of videos at a time, makes the job execute faster:
 			$upload_limit = 2;
-			
+
 			// Upload files:
 			$videos = $db->fetch(sprintf(
 				'
@@ -60,7 +60,7 @@
 				',
 				$upload_limit
 			));
-			
+
 			if (is_array($videos) and !empty($videos)) foreach ($videos as $data) {
 				$name = basename($data['file']);
 				$id = $api->createMedia('video', $data['file'], array(
@@ -69,7 +69,7 @@
 					'linkText'			=> $conf->get('sitename', 'general'),
 					'linkURL'			=> URL
 				));
-				
+
 				if ($id) {
 					$data['video_id'] = $id;
 					$data['uploading'] = 'yes';
@@ -78,19 +78,19 @@
 						$data['entry_id'], $data['video_id'], basename($data['file'])
 					);
 				}
-				
+
 				else {
 					$data['attempts']++;
-					
+
 					if ($data['attempts'] >= $max_attempts) {
 						$data['failed'] = 'yes';
 					}
 				}
-				
+
 				###
 				# Delegate: BrightCove_VideoCreated
 				# Description: Allow data changes and api calls after a video is created.
-				$page->ExtensionManager->notifyMembers(
+				Symphony::ExtensionManager()->notifyMembers(
 					'BrightCove_VideoCreated',
 					'/publish/', array(
 						'api'		=> $api,
@@ -100,10 +100,10 @@
 						'page'		=> $page
 					)
 				);
-				
+
 				$db->insert($data, 'tbl_brightcove', true);
 			}
-			
+
 			// Check status:
 			$videos = $db->fetch("
 				SELECT
@@ -119,14 +119,14 @@
 						d.edited = 'yes'
 					)
 			");
-			
+
 			if (is_array($videos) and !empty($videos)) foreach ($videos as $data) {
 				$status = $api->getStatus('video', $data['video_id']);
 				$data['completed'] = 'no';
 				$data['encoding'] = 'no';
 				$data['uploading'] = 'no';
 				$data['edited'] = 'no';
-				
+
 				if ($status == 'UPLOADING') {
 					$data['uploading'] = 'yes';
 					$stats['uploading'][] = sprintf(
@@ -134,7 +134,7 @@
 						$data['entry_id'], $data['video_id'], basename($data['file'])
 					);
 				}
-				
+
 				else if ($status == 'PROCESSING') {
 					$data['encoding'] = 'yes';
 					$stats['encoding'][] = sprintf(
@@ -142,7 +142,7 @@
 						$data['entry_id'], $data['video_id'], basename($data['file'])
 					);
 				}
-				
+
 				else if ($status == 'COMPLETE') {
 					$data['completed'] = 'yes';
 					$stats['completed'][] = sprintf(
@@ -150,15 +150,15 @@
 						$data['entry_id'], $data['video_id'], basename($data['file'])
 					);
 				}
-				
+
 				else {
 					$data['failed'] = 'yes';
 				}
-				
+
 				###
 				# Delegate: BrightCove_VideoUpdated
 				# Description: Allow data changes and api calls after a video is created.
-				$page->ExtensionManager->notifyMembers(
+				Symphony::ExtensionManager()->notifyMembers(
 					'BrightCove_VideoUpdated',
 					'/publish/', array(
 						'api'		=> $api,
@@ -168,23 +168,23 @@
 						'page'		=> $page
 					)
 				);
-				
+
 				$db->insert($data, 'tbl_brightcove', true);
 				$status = $driver->getVideoStatus($data['entry_id']);
 				$driver->setVideoStatus($data['entry_id'], $status);
 			}
-			
+
 			$template = "
 %d videos uploading (%d just started):
 	%s
-	
+
 %d videos are being encoded:
 	%s
 
 %d videos have been finished:
 	%s
 			";
-			
+
 			printf(
 				trim($template),
 				count($stats['uploading']),
@@ -195,9 +195,9 @@
 				count($stats['completed']),
 				implode("\n\t", $stats['completed'])
 			);
-			
+
 			exit;
 		}
 	}
-	
+
 ?>
--- extension.driver.php
+++ extension.driver.php
@@ -1,42 +1,42 @@
 <?php
-	
+
 	require_once(EXTENSIONS . '/brightcove/lib/echove.php');
-	
+
 	class Extension_Brightcove extends Extension {
 	/*-------------------------------------------------------------------------
 		Definition:
 	-------------------------------------------------------------------------*/
-		
+
 		public function about() {
 			return array(
 				'name'			=> 'Brightcove',
-				'version'		=> '1.0.1',
-				'release-date'	=> '2010-01-12',
+				'version'		=> '1.2',
+				'release-date'	=> '2011-07-29',
 				'author'		=> array(
 					'name'			=> 'Rowan Lewis',
 					'website'		=> 'http://rowanlewis.com/',
 					'email'			=> 'me@rowanlewis.com'
 				),
-				'description'	=> 'Adds Bright Cove video upload support to upload fields.'
+				'description'	=> 'Adds Bright Cove video upload support to Advanced Upload fields.'
 			);
 		}
-		
+
 		public function uninstall() {
-			//$this->_Parent->Database->query("DROP TABLE `tbl_fields_brightcove_status`");
-			//$this->_Parent->Database->query("DROP TABLE `tbl_brightcove`");
+			Symphony::Database()->query("DROP TABLE `tbl_fields_brightcove_status`");
+			Symphony::Database()->query("DROP TABLE `tbl_brightcove`");
 		}
-		
+
 		public function install() {
-			$this->_Parent->Database->query("
+			Symphony::Database()->query("
 				CREATE TABLE IF NOT EXISTS `tbl_fields_brightcove_status` (
 					`id` int(11) unsigned NOT NULL auto_increment,
 					`field_id` int(11) unsigned NOT NULL,
 					PRIMARY KEY  (`id`),
 					KEY `field_id` (`field_id`)
-				)
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 			");
-			
-			$this->_Parent->Database->query("
+
+			Symphony::Database()->query("
 				CREATE TABLE IF NOT EXISTS `tbl_brightcove` (
 					`id` int(11) NOT NULL auto_increment,
 					`entry_id` int(11) NOT NULL,
@@ -52,27 +52,27 @@
 					KEY `entry_id` (`entry_id`),
 					KEY `field_id` (`field_id`),
 					KEY `video_id` (`video_id`)
-				)
+				) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 			");
-			
+
 			return true;
 		}
-		
+
 		public function getSubscribedDelegates() {
 			return array(
 				array(
-					'page'		=> '/frontend/',
-					'delegate'	=> 'UploadField_AppendFormattedElement',
+					'page'		=> '/extension/advanced_upload_field/',
+					'delegate'	=> 'AppendFormattedElement',
 					'callback'	=> 'appendFormattedElement'
 				),
 				array(
-					'page'		=> '/publish/',
-					'delegate'	=> 'UploadField_AppendMediaPreview',
+					'page'		=> '/extension/advanced_upload_field/',
+					'delegate'	=> 'AppendMediaPreview',
 					'callback'	=> 'appendMediaPreview'
 				),
 				array(
-					'page'		=> '/publish/',
-					'delegate'	=> 'UploadField_PostProccessFile',
+					'page'		=> '/extension/advanced_upload_field/',
+					'delegate'	=> 'PostProcessFile',
 					'callback'	=> 'postProccessFile'
 				),
 				array(
@@ -82,62 +82,62 @@
 				)
 			);
 		}
-		
+
 	/*-------------------------------------------------------------------------
 		Preferences:
 	-------------------------------------------------------------------------*/
-		
+
 		public function getPreferencesData() {
-			$config = $this->_Parent->Configuration;
+			$config = Symphony::Configuration();
 			$data = array(
 				'read-api-key'			=> '',
 				'write-api-key'			=> '',
 				'backend-player-id'		=> '',
 				'frontend-player-id'	=> ''
 			);
-			
+
 			foreach ($data as $key => &$value) {
 				$value = $config->get($key, 'brightcove');
 			}
-			
+
 			return $data;
 		}
-		
+
 		public function getReadApiKey() {
-			$config = $this->_Parent->Configuration;
-			
+			$config = Symphony::Configuration();
+
 			return $config->get('read-api-key', 'brightcove');
 		}
-		
+
 		public function getWriteApiKey() {
-			$config = $this->_Parent->Configuration;
-			
+			$config = Symphony::Configuration();
+
 			return $config->get('write-api-key', 'brightcove');
 		}
-		
+
 		public function getBackendPlayerId() {
-			$config = $this->_Parent->Configuration;
-			
+			$config = Symphony::Configuration();
+
 			return $config->get('backend-player-id', 'brightcove');
 		}
-		
+
 		public function getFrontendPlayerId() {
-			$config = $this->_Parent->Configuration;
-			
+			$config = Symphony::Configuration();
+
 			return $config->get('frontend-player-id', 'brightcove');
 		}
-		
+
 		public function addCustomPreferenceFieldsets($context) {
 			$data = $this->getPreferencesData();
-			$page = Administration::instance()->Page;
+			$page = Symphony::Engine()->Page;
 			$page->addStylesheetToHead(
 				URL . '/extensions/neatify/assets/preferences.css', 'screen', 250
 			);
-			
+
 			$fieldset = new XMLElement('fieldset');
 			$fieldset->setAttribute('class', 'settings');
 			$fieldset->appendChild(new XMLElement('legend', 'Brightcove'));
-			
+
 			$this->buildPreferences(
 				__('Interface'), $fieldset,
 				array(
@@ -163,77 +163,64 @@
 					)
 				)
 			);
-			
+
 			$context['wrapper']->appendChild($fieldset);
 		}
-		
+
 		public function buildPreferences($title, $fieldset, $data) {
 			$row = null;
-			
+
 			foreach ($data as $index => $item) {
 				if ($index % 2 == 0) {
 					if ($row) $fieldset->appendChild($row);
-					
+
 					$row = new XMLElement('div');
 					$row->setAttribute('class', 'group');
 				}
-				
+
 				$label = Widget::Label(__($item['label']));
 				$name = 'settings[brightcove][' . $item['name'] . ']';
-				
+
 				$input = Widget::Input($name, $item['value']);
-				
+
 				$label->appendChild($input);
 				$row->appendChild($label);
 			}
-			
+
 			$fieldset->appendChild($row);
 		}
-		
+
 	/*-------------------------------------------------------------------------
 		Utilites:
 	-------------------------------------------------------------------------*/
-		
+
 		protected $apppendPublishHeaders = false;
 		protected $apppendSettingsHeaders = false;
-		
+
 		public function apppendPublishHeaders() {
 			$admin = Administration::instance();
-			
+
 			if ($admin->Page and !$this->apppendPublishHeaders) {
 				$admin->Page->addStylesheetToHead(URL . '/extensions/brightcove/assets/publish.css', 'screen', 7126310);
 				$admin->Page->addScriptToHead('http://admin.brightcove.com/js/BrightcoveExperiences.js', 7126310);
-				
+
 				$this->apppendPublishHeaders = true;
 			}
 		}
-		
+
 		public function addSettingsHeaders() {
-			$admin = Administration::instance();
-			
-			if ($admin->Page and !$this->apppendSettingsHeaders) {
-				//$admin->Page->addStylesheetToHead(URL . '/extensions/brightcove/assets/publish.css', 'screen', 7126310);
-				//$admin->Page->addScriptToHead('http://admin.brightcove.com/js/BrightcoveExperiences.js', 7126310);
-				
+			if (Symphony::Engine()->Page && !$this->apppendSettingsHeaders) {
 				$this->apppendSettingsHeaders = true;
 			}
 		}
-		
+
 		public function getAPI() {
 			return new Echove($this->getReadApiKey(), $this->getWriteApiKey());
 		}
-		
+
 		public function setVideoStatus($entry_id, $status) {
-			if (class_exists('Administration')) {
-				$symphony = Administration::instance();
-			}
-			
-			else {
-				$symphony = Frontend::instance();
-			}
-			
-			$db = $symphony->Database;
-			
+			$db = Symphony::Database();
+
 			// Update status field:
 			$field_id = $db->fetchVar('id', 0, sprintf(
 				'
@@ -250,9 +237,9 @@
 				',
 				$entry_id
 			));
-			
+
 			if (!$field_id) return false;
-			
+
 			$data = $db->fetchRow(0, sprintf(
 				'
 					SELECT
@@ -265,23 +252,23 @@
 				',
 				$field_id, $entry_id
 			));
-			
+
 			$data['entry_id'] = $entry_id;
 			$data['handle'] = $status;
 			$data['value'] = ucwords($status);
-			
+
 			return $db->insert($data, "tbl_entries_data_{$field_id}", true);
 		}
-		
+
 		public function getVideoStatus($entry_id) {
 			if (class_exists('Administration')) {
 				$symphony = Administration::instance();
 			}
-			
+
 			else {
 				$symphony = Frontend::instance();
 			}
-			
+
 			$db = $symphony->Database;
 			$data = $db->fetchRow(0, sprintf(
 				"
@@ -295,39 +282,39 @@
 				",
 				$entry_id
 			));
-			
+
 			if (empty($data)) {
 				return 'none';
 			}
-			
+
 			if ($data['completed'] == 'yes') {
 				return 'completed';
 			}
-			
+
 			else if ($data['failed'] == 'yes') {
 				return 'failed';
 			}
-			
+
 			else if ($data['encoding'] == 'yes') {
 				return 'encoding';
 			}
-			
+
 			else if ($data['uploading'] == 'yes') {
 				return 'uploading';
 			}
-			
+
 			else {
 				return 'queued';
 			}
 		}
-		
+
 		public function appendFormattedElement($context) {
 			$db = $context['parent']->Database;
 			$data = $context['data'];
 			$wrapper = $context['wrapper'];
-			
+
 			if (!preg_match('/^video\//i', $data['mimetype'])) return;
-			
+
 			$row = $db->fetchRow(0, sprintf(
 				"
 					SELECT
@@ -341,7 +328,7 @@
 				$context['entry_id'],
 				$context['field_id']
 			));
-			
+
 			// Show preview:
 			if ($row['completed'] == 'yes') {
 				$embed = new XMLElement('brightcove');
@@ -361,14 +348,14 @@
 				$wrapper->appendChild($embed);
 			}
 		}
-		
+
 		public function appendMediaPreview($context) {
 			$db = $context['parent']->Database;
 			$data = $context['data'];
 			$wrapper = $context['wrapper'];
-			
+
 			if (!preg_match('/^video\//i', $data['mimetype'])) return;
-			
+
 			$row = $db->fetchRow(0, sprintf(
 				"
 					SELECT
@@ -382,11 +369,11 @@
 				$context['entry_id'],
 				$context['field_id']
 			));
-			
+
 			// Show preview:
 			if ($row['completed'] == 'yes') {
 				$this->apppendPublishHeaders();
-				
+
 				$embed = new XMLElement('div');
 				$embed->setAttribute('class', 'brightcove-video-embed');
 				$embed->setValue(sprintf(
@@ -405,13 +392,13 @@
 				$wrapper->appendChild($embed);
 			}
 		}
-		
+
 		public function postProccessFile($context) {
-			$db = $context['parent']->Database;
+			$db = Symphony::Database();
 			$data = $context['data'];
-			
+
 			if (!preg_match('/^video\//i', $data['mimetype'])) return;
-			
+
 			$row = $db->fetchRow(0, sprintf(
 				"
 					SELECT
@@ -425,7 +412,7 @@
 				$context['entry_id'],
 				$context['field_id']
 			));
-			
+
 			$db->insert(
 				array(
 					'id'		=> @$row['id'],
@@ -442,5 +429,5 @@
 			);
 		}
 	}
-	
+
 ?>
